0ceae09d0ea5ee31a0b330c061bb521c6c239d5a
Your Name <you@example.com>
2025-07-09 16:11:48 +0000

    nof lab 102

--- AWS/main.tf
+++ AWS/main.tf
@@ -27,7 +27,21 @@ resource "aws_instance" "vm" {
   }
 }
 
+resource "null_resource" "check_public_ip" {
+  provisioner "local-exec" {
+    command = <<EOT
+      if [ -z "${aws_instance.vm.public_ip}" ]; then
+        echo "ERROR: Public IP address was not assigned." >&2
+        exit 1
+      fi
+    EOT
+  }
+  depends_on = [aws_instance.vm]
+}
+
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
   description = "Public IP address of the VM"
+  depends_on = [null_resource.check_public_ip]
+
 }
--- AWS/providers.tf
+++ AWS/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "6.2.0"
     }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.4"
+    }
   }
 }
 
